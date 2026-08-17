b134b7416efa6795788fa3784a3ac63a4c17808f
benedikt <benedikt.m.gudmundsson@gmail.com>
2026-08-17 16:42:48 +0000

    Fjarlaegja ovart hreidrada storefront moppu

--- storefront/storefront/.next/dev/static/chunks/_0zrmhfx._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/1-4i_next_dist_0x9o9av._.js","static/chunks/[root-of-the-server]__08gnkw3._.js"],
-    source: "entry"
-});
